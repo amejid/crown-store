d27fe8162f88ef22df8e39156cfcc10c81438a32
amejid <abdelmejid89@gmail.com>
2022-11-25 19:35:05 +0300

    Implement cart dropdown and toggle

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Home from "./routes/home/home.component";
 import {Route, Routes} from "react-router-dom";
 import Navigation from "./routes/navigation/navigation.component";
 import Authentication from "./routes/authentication/authentication.component";
+import Shop from "./routes/shop/shop.component";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Route path="/" element={<Navigation/>}>
         <Route index element={<Home/>}/>
         <Route path='auth' element={<Authentication/>}/>
+        <Route path='shop' element={<Shop/>}/>
       </Route>
     </Routes>
   )
--- src/routes/navigation/navigation.component.js
+++ src/routes/navigation/navigation.component.js
@@ -1,12 +1,16 @@
 import {Link, Outlet} from "react-router-dom";
 import {ReactComponent as CrownLogo} from '../../assets/crown.svg'
 import './navigation.styles.scss'
-import {useContext} from "react";
+import {useContext, useState} from "react";
 import {UserContext} from "../../context/user.context";
 import {signOutUser} from "../../utils/firebase/firebase.utils";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+import {CartContext} from "../../context/cart.context";
 
 const Navigation = () => {
   const {currentUser} = useContext(UserContext)
+  const {isCartOpen, setIsCartOpen} = useContext(CartContext)
 
   return (
     <>
@@ -21,7 +25,9 @@ const Navigation = () => {
           {currentUser ? (<span className="nav-link" onClick={signOutUser}>SIGN OUT</span>) :
             <Link className='nav-link' to='/auth'>SIGN IN</Link>
           }
+          <CartIcon />
         </div>
+        {isCartOpen && <CartDropdown/>}
       </div>
       <Outlet/>
     </>
