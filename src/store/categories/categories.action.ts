8b164dd8ef31b165d77319ee63880459b2dd29e2
amejid <abdelmejid89@gmail.com>
2022-11-29 09:35:40 +0300

    Install typescript

--- src/store/categories/categories.action.ts
+++ src/store/categories/categories.action.ts
@@ -1,4 +1,4 @@
-import createAction from '../../utils/reducer/reducer.utils';
+import { createAction } from '../../utils/reducer/reducer.utils';
 import CATEGORIES_ACTION_TYPES from './categories.types';
 
 export const fetchCategoriesStart = () => (
--- src/store/categories/categories.reducer.ts
+++ src/store/categories/categories.reducer.ts
@@ -1,4 +1,4 @@
-import CATEGORIES_ACTION_TYPES from './categories.types';
+import { CATEGORIES_ACTION_TYPES } from './categories.types';
 
 const INITIAL_STATE = {
   categories: [],
