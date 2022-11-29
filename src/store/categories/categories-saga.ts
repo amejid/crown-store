2024640a3b9c93bab38b70377d117f06322ea6c7
amejid <abdelmejid89@gmail.com>
2022-11-29 12:27:44 +0300

    Migrate sagas

--- src/store/categories/categories-saga.ts
+++ src/store/categories/categories-saga.ts
@@ -1,23 +1,23 @@
 import {
   takeLatest, all, call, put,
-} from 'redux-saga/effects';
+} from 'typed-redux-saga/macro';
 import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
 import { fetchCategoriesFailed, fetchCategoriesSuccess } from './categories.action';
 import { CATEGORIES_ACTION_TYPES } from './categories.types';
 
 export function* fetchCategoriesAsync() {
   try {
-    const categoriesArray = yield call(getCategoriesAndDocuments, 'categories');
-    yield put(fetchCategoriesSuccess(categoriesArray));
+    const categoriesArray = yield* call(getCategoriesAndDocuments);
+    yield* put(fetchCategoriesSuccess(categoriesArray));
   } catch (error) {
-    yield put(fetchCategoriesFailed(error));
+    yield* put(fetchCategoriesFailed(error as Error));
   }
 }
 
 export function* onFetchCategories() {
-  yield takeLatest(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START, fetchCategoriesAsync);
+  yield* takeLatest(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START, fetchCategoriesAsync);
 }
 
 export function* categoriesSaga() {
-  yield all([call(onFetchCategories)]);
+  yield* all([call(onFetchCategories)]);
 }
--- src/store/root-saga.ts
+++ src/store/root-saga.ts
@@ -1,7 +1,7 @@
-import { all, call } from 'redux-saga/effects';
+import { all, call } from 'typed-redux-saga/macro';
 import { categoriesSaga } from './categories/categories-saga';
 import { userSaga } from './user/user.saga';
 
 export default function* rootSaga() {
-  yield all([call(categoriesSaga), call(userSaga)]);
+  yield* all([call(categoriesSaga), call(userSaga)]);
 }
